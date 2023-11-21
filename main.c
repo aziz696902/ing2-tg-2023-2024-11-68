31bd9a494da81151f07480ae26f9ed8babb45d94
talia <talia.toutah@edu.ece.fr>
2023-11-21 10:07:19 +0100

    creation main

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by talia on 21/11/2023.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by talia on 21/11/2023.
+//
+
+#ifndef ING2_TG_2023_2024_11_68_MAIN_H
+#define ING2_TG_2023_2024_11_68_MAIN_H
+
+#endif //ING2_TG_2023_2024_11_68_MAIN_H
